894c86dc9a731380d833ebd868642501f92f405a
gouraviwdev-hue <gourav.iwdev@gmail.com>
2026-03-26 19:52:26 +0530

    added app icon for android

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux';
 import { store } from './src/redux/store';
 import AppNavigator from './src/navigation/AppNavigator';
 import { StatusBar } from 'react-native';
+import BootSplash from 'react-native-bootsplash';
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { checkAndRequestAllPermissions } from './src/utils/permissions';
@@ -12,6 +13,7 @@ const App = () => {
     const init = async () => {
       try {
         // Wait for the app to be ready
+        BootSplash.hide({ fade: true });
         await Promise.all([
           checkAndRequestAllPermissions(),
           // Add any other initialization tasks here
